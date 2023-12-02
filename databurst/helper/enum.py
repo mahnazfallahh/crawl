fcf49711e2c64a2513cee3250185134f66f9742f
mahnazfallahh <mahnaz.fallah1213@gmail.com>
2023-12-02 21:49:12 +0330

    refactor:enum file name field

--- databurst/helper/enum.py
+++ databurst/helper/enum.py
@@ -8,8 +8,10 @@ class SpiderInfo(Enum):
     Enum Values:
         NAME (str): The name of the spider.
         START_URLS (list of str): A list of URLs to start crawling.
+        FILE_NAME (str): CSV file name .
         PATH (str): The XPath selector to select specific elements from the web pages.
     """
     NAME = "databurst_spider"
+    FILE_NAME = "databurst/ducs/text.csv"
     START_URLS = ["https://databurst.ir"]
     PATH = '//*[(@id = "features")]//*[contains(concat( " ", @class, " " ), concat( " ", "text-base", " " )) and contains(concat( " ", @class, " " ), concat( " ", "text-gray-500", " " ))]'
