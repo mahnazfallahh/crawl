ba906b36515ac834ac6648e334e228a300b5f32e
mahnazfallahh <mahnaz.fallah1213@gmail.com>
2023-12-02 21:49:59 +0330

    refactor:store crawl data in csv file

--- databurst/spiders/databurst_spider.py
+++ databurst/spiders/databurst_spider.py
@@ -1,8 +1,10 @@
+import csv
 import scrapy
 # import logging.config
 from scrapy.loader import ItemLoader
 from databurst.items import DataburstItem
 from databurst.helper.enum import SpiderInfo
+from databurst.helper.messages import CsvMessage
 
 
 # logging.config.fileConfig('databurst/config/log/config.toml')
@@ -10,29 +12,26 @@ from databurst.helper.enum import SpiderInfo
 
 class DataburstSpider(scrapy.Spider):
     """
-    A Scrapy spider for scraping data from web pages using XPath selectors.
-    It extracts text content from specific elements on the page and stores it in a DataburstItem.
+    A Scrapy spider for scraping data and writing it to a CSV file.
 
     Attributes:
         name (str): The name of the spider.
-        start_urls (list of str): A list of URLs to start crawling.
-
-    Methods:
-        parse(response): The main method that handles the response received from the start URLs.
+        start_urls (list): A list of URLs to start the spider's crawling process.
+        file_name (str): The name of the CSV file.
     """
     name = SpiderInfo.NAME.value
     start_urls = SpiderInfo.START_URLS.value
+    file_name = SpiderInfo.FILE_NAME.value
     
     def parse(self, response):
         """
-        Parse the response received from the start URLs.
-        Extracts data from the response using XPath selectors and stores it in a DataburstItem.
-        
-        Args:
-            response: The response received from the start URLs.
+        Parse the response and extract data from the web page.
 
-        Yields:
-            DataburstItem: An item containing the extracted data.
+        Args:
+            response: The response received from the web page.
+
+        Raises:
+            csv.Error: If an error occurs while writing to the CSV file.
         """
         # logger.info("Parsing Is Started ...")
         elements = response.xpath(SpiderInfo.PATH.value)
@@ -40,3 +39,9 @@ class DataburstSpider(scrapy.Spider):
             loader = ItemLoader(item=DataburstItem(), selector=element)
             loader.add_xpath('text_content', 'string()')
             yield loader.load_item()
+            try:
+                with open(self.file_name, 'a') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(loader.get_collected_values('text_content'))
+            except csv.Error:
+                print(CsvMessage.CSV_ERROR)
